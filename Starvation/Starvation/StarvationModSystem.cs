878ae9263155ad0101769f797945d67dd8d15473
maxvzd <153630971+maxvzd@users.noreply.github.com>
2025-12-30 22:19:03 +0000

    init

---
+++ Starvation/Starvation/StarvationModSystem.cs
@@ -0,0 +1,10 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace Starvation;
+
+public class StarvationModSystem : ModSystem
+{
+}
